fe33a8d6feadb52373b3e3fbc35617feff2f5166
Kaung Chit Thet <lovesniper6@gmail.com>
2018-07-02 10:14:02 +0630

    Change database

--- index.php
+++ index.php
@@ -1,6 +1,41 @@
 <?php include "partials/header.php" ?>
 <?php include "partials/navbar.php" ?>
 	<div class="conatiner">
-		<h1>Hi</h1>
+		<h1 class="text-center">Company</h1>
+		<div class="col-md-8 col-md-offset-2">
+
+			<table class="table">
+			<tr>
+				<th>No</th><th>Name</th><th>Address</th><th>Phone</th><th></th>
+			</tr>	
+			<?php 
+					$db=new mysqli("localhost","root","","companyproject");
+					
+					if (isset($_GET["search"]))
+					{
+						$value=$_GET["search"];
+						$query="select * from company where name Like '".$value."%'";	
+					}
+					else
+					{
+						$query="select * from company";	
+					}
+					
+					
+
+					$result=$db->query($query);
+					while ($c=$result->fetch_object())
+					{
+						echo '<tr>
+								<td>'.$c->id.'</td><td>'.$c->name.'</td><td>'.$c->address.'</td><td>'.$c->phone.'</td><td>
+								<a href="" class="btn btn-info">Edit</a><a href="" class="btn btn-danger">Delete</a>
+								</td>
+							</tr>';
+					}
+			 ?>
+			
+			
+			</table>	
+		</div>
 	</div>
 <?php include "partials/footer.php" ?>
--- partials/header.php
+++ partials/header.php
@@ -2,5 +2,5 @@
 <head>
 	<title>Blog Project</title>
 	<link rel="stylesheet" type="text/css" href="public/bootstrap/css/bootstrap.css">
-	<link rel="stylesheet" type="text/css" href="public/css/style.css">
+	
 </head>
